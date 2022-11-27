ac178d4b04919fa9f36136fbe0b68fe0ef8cf37c
Ryan Gasparini <ryan@rxgx.com>
2022-11-27 09:37:16 +0000

    chore: update dev packages?

--- packages/button/src/button.test.jsx
+++ packages/button/src/button.test.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { fireEvent, render, screen } from '@testing-library/react'
 import Button from './button.jsx'
 
+const buttonText = 'BUTTON TEXT'
+
 it('renders', () => {
   const spyOnClick = jest.fn()
-  render(<Button onClick={spyOnClick}>BUTTON TEXT</Button>)
+  render(<Button onClick={spyOnClick}>{buttonText}</Button>)
 
-  const subject = screen.getByText('BUTTON TEXT')
+  const subject = screen.getByText(buttonText)
   expect(subject).toBeInTheDocument()
   fireEvent.click(subject, { target: 'JavaScript' })
   expect(spyOnClick).toBeCalled()
